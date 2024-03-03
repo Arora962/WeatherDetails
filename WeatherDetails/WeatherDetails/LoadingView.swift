e6d7a06c56d9ea593e26927e24ee392b84e91288
Arora962 <157892517+Arora962@users.noreply.github.com>
2024-03-03 22:55:23 +0530

    Add files via upload

---
+++ WeatherDetails/WeatherDetails/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Kriti Arora on 03/03/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
---
+++ WeatherDetails/WeatherDetails/WeatherDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDetailsApp.swift
+//  WeatherDetails
+//
+//  Created by Kriti Arora on 03/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
